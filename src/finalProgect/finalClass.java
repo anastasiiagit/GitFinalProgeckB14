b47b73a1cf02619aaec84d0e73e6d6a99a6b9b67
Anastasiia Balaniuk <anastasiiabalaniuk@anastasiias-air.myfiosgateway.com>
2022-09-28 20:11:35 -0400

    first commit

---
+++ src/finalProgect/finalClass.java
@@ -0,0 +1,10 @@
+package finalProgect;
+
+public class finalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author anastasiiabalaniuk
+ *
+ */
+module FinalGitProgect {
+}
